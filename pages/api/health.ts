10ce9a3e354bf3dc235eb62d0df432c10b8c4505
tomek-i <tomek.iwainski@gmail.com>
2023-12-19 00:49:47 +1100

    removed files

--- pages/api/health.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default function handler(_req: NextApiRequest, res: NextApiResponse) {
-  ok(res, { status: "ok" });
-}
-
-const ok = (res: NextApiResponse, args: unknown) => {
-  res.status(200).json(args);
-};
